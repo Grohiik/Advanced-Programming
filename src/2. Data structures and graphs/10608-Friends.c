bff64265950e5d8086282ccbe58a964c9f6d250b
Linnéa <heartbrrrrr@protonmail.com>
2022-09-02 12:42:18 +0200

    Add Node struct

--- src/2. Data structures and graphs/10608-Friends.c
+++ src/2. Data structures and graphs/10608-Friends.c
@@ -1,1 +1,51 @@
 /* https://onlinejudge.org/external/106/10608.pdf */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_NODES 30000
+
+typedef struct Node {
+    int num_friends;
+    int rank;
+    Node *parent;
+} Node;
+
+void init_nodes(int n, Node **nodes) {
+    int i = 0;
+    Node *current_node;
+    for (i; i < n; i++) {
+        nodes[i] = (Node *)malloc(sizeof(Node));
+        current_node = nodes[i];
+        current_node->num_friends = 1;
+        current_node->rank = 0;
+        current_node->parent = current_node;
+    }
+}
+
+int main() {
+    int people, links;
+    int person1, person2;
+    int times;
+    scanf("%d", &times);
+
+    while (times--) {
+
+        scanf("%d%d", &people, &links);
+
+        Node **nodes = (Node *)malloc(people * sizeof(Node));
+        init_nodes(people, nodes);
+
+        int max_friend_circle = 1;
+
+        while (links--) {
+            scanf("%d%d", person1, person2);
+        }
+
+        int i = 0;
+        for (i; i < people; i++) {
+            free(nodes[i]);
+        }
+        free(nodes);
+    }
+}
