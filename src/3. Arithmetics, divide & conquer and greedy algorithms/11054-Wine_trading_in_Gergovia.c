873cc4926585fc982189ed0bfa657a9120a64497
Linnea <heartbrrrrr@protonmail.com>
2022-09-05 12:25:42 +0200

    Start work on uva 11054

--- src/3. Arithmetics, divide & conquer and greedy algorithms/11054-Wine_trading_in_Gergovia.c
+++ src/3. Arithmetics, divide & conquer and greedy algorithms/11054-Wine_trading_in_Gergovia.c
@@ -1,4 +1,21 @@
 /*
 https://onlinejudge.org/external/110/11054.pdf
-
 */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int inhabitants;
+    scanf("%d", &inhabitants);
+    while (inhabitants) {
+        int needs_to_be_moved = 0;
+        long long total_work = 0;
+        int current_house_request;
+        while (inhabitants--) {
+            scanf("%d", &current_house_request);
+            needs_to_be_moved += current_house_request;
+            total_work += abs(needs_to_be_moved);
+        }
+        scanf("%d", &inhabitants);
+    }
+}
