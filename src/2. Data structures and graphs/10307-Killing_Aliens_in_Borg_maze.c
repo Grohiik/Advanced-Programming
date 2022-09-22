3cdd85035c488440890ea58e536bacffbb1417d3
Linnea <heartbrrrrr@protonmail.com>
2022-09-22 12:09:31 +0200

    work in progress borg maze

--- src/2. Data structures and graphs/10307-Killing_Aliens_in_Borg_maze.c
+++ src/2. Data structures and graphs/10307-Killing_Aliens_in_Borg_maze.c
@@ -5,7 +5,7 @@ https://onlinejudge.org/external/103/10307.pdf
 #include <stdio.h>
 #include <stdlib.h>
 
-int bfs() {}
+int bfs() { return 0; }
 
 int main() {
     int cases;
@@ -13,15 +13,26 @@ int main() {
     int width, height;
     while (cases--) {
         scanf("%d%d", &width, &height);
+        int start[2];
+        int end[50][2];
+        int alien_counter = 0;
         char **blub = (char *)malloc(sizeof(char *) * width);
         for (int i = 0; i < width; i++) {
             blub[i] = (char *)malloc(sizeof(char) * height);
         }
 
         for (int i = 0; i < height; i++) {
+            scanf("%s", &blub[i]);
             for (int ii = 0; ii < width; ii++) {
-                scanf("%c", &blub[i][ii]);
+                if (blub[i][ii] == 'S') {
+                    start[0] = i;
+                    start[1] = ii;
+                } else if (blub[i][ii] == 'A') {
+                    end[alien_counter][0] = i;
+                    end[alien_counter++][1] = ii;
+                }
             }
         }
+        printf("%d %d", start[0], start[1]);
     }
 }
