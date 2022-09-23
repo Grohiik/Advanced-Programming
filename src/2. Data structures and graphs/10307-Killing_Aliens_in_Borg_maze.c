1d58ed8e5c2992f50d1e31654e31df1913b47e34
Linnéa <heartbrrrrr@protonmail.com>
2022-09-23 10:52:46 +0200

    Restructure of Killing_Aliens_in_Borg_maze

--- src/2. Data structures and graphs/10307-Killing_Aliens_in_Borg_maze.c
+++ src/2. Data structures and graphs/10307-Killing_Aliens_in_Borg_maze.c
@@ -7,32 +7,40 @@ https://onlinejudge.org/external/103/10307.pdf
 
 int bfs() { return 0; }
 
+void init_matrix(char ***matrix, int width, int height) {
+    *matrix = (char *)calloc(width, sizeof(char *));
+    for (int i = 0; i < width; i++) {
+        *matrix[i] = (char *)calloc(height, sizeof(char));
+    }
+}
+
+int read_maze(char ***matrix, int width, int height, int **start, int ***end) {
+    int alien_counter = 0;
+    for (int i = 0; i < height; i++) {
+        scanf("%s", &(*matrix[i]));
+        for (int ii = 0; ii < width; ii++) {
+            if (*matrix[i][ii] == 'S') {
+                *start[0] = i;
+                *start[1] = ii;
+            } else if (*matrix[i][ii] == 'A') {
+                *end[alien_counter][0] = i;
+                *end[alien_counter++][1] = ii;
+            }
+        }
+    }
+}
+
 int main() {
     int cases;
     scanf("%d", &cases);
     int width, height;
+    char **maze;
     while (cases--) {
         scanf("%d%d", &width, &height);
         int start[2];
         int end[50][2];
-        int alien_counter = 0;
-        char **blub = (char *)malloc(sizeof(char *) * width);
-        for (int i = 0; i < width; i++) {
-            blub[i] = (char *)malloc(sizeof(char) * height);
-        }
 
-        for (int i = 0; i < height; i++) {
-            scanf("%s", &blub[i]);
-            for (int ii = 0; ii < width; ii++) {
-                if (blub[i][ii] == 'S') {
-                    start[0] = i;
-                    start[1] = ii;
-                } else if (blub[i][ii] == 'A') {
-                    end[alien_counter][0] = i;
-                    end[alien_counter++][1] = ii;
-                }
-            }
-        }
-        printf("%d %d", start[0], start[1]);
+        init_matrix(&maze, width, height);
+        int alien_counter = read_maze(&maze, width, height, &start, &end);
     }
 }
