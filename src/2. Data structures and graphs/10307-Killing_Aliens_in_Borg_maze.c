2bb80983bcea98e4af84c994ca997fdf8c731422
Linnéa <heartbrrrrr@protonmail.com>
2022-09-23 11:46:58 +0200

    Pointers hard
    Messed up width and height
    Made realloc call instead of calloc
    Fix other pointer shit

--- src/2. Data structures and graphs/10307-Killing_Aliens_in_Borg_maze.c
+++ src/2. Data structures and graphs/10307-Killing_Aliens_in_Borg_maze.c
@@ -7,24 +7,24 @@ https://onlinejudge.org/external/103/10307.pdf
 
 int bfs() { return 0; }
 
-void init_matrix(char ***matrix, int width, int height) {
-    *matrix = (char *)calloc(width, sizeof(char *));
-    for (int i = 0; i < width; i++) {
-        *matrix[i] = (char *)calloc(height, sizeof(char));
+void init_matrix(char **matrix, int width, int height) {
+    matrix = (char **)realloc(matrix, height * sizeof(char *));
+    for (int i = 0; i < height; i++) {
+        matrix[i] = (char *)realloc(matrix[i], width * sizeof(char));
     }
 }
 
-int read_maze(char ***matrix, int width, int height, int **start, int ***end) {
+int read_maze(char **matrix, int width, int height, int *start, int **end) {
     int alien_counter = 0;
     for (int i = 0; i < height; i++) {
-        scanf("%s", &(*matrix[i]));
+        scanf("%s", matrix[i]);
         for (int ii = 0; ii < width; ii++) {
-            if (*matrix[i][ii] == 'S') {
-                *start[0] = i;
-                *start[1] = ii;
-            } else if (*matrix[i][ii] == 'A') {
-                *end[alien_counter][0] = i;
-                *end[alien_counter++][1] = ii;
+            if (matrix[i][ii] == 'S') {
+                start[0] = i;
+                start[1] = ii;
+            } else if (matrix[i][ii] == 'A') {
+                end[alien_counter][0] = i;
+                end[alien_counter++][1] = ii;
             }
         }
     }
@@ -40,7 +40,13 @@ int main() {
         int start[2];
         int end[50][2];
 
-        init_matrix(&maze, width, height);
-        int alien_counter = read_maze(&maze, width, height, &start, &end);
+        init_matrix(maze, width, height);
+        int alien_counter = read_maze(maze, width, height, start, end);
+        printf("%d %d", start[0], start[1]);
     }
+
+    for (int i = 0; i < height; i++) {
+        free(maze[i]);
+    }
+    free(maze);
 }
